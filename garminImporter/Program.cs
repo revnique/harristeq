9ca2529d2a00d7dc407cfa16dd5448bda0522324
Dominique <dharris@defisolutions.com>
2014-12-28 00:27:12 -0600

    started on the converter

--- garminImporter/Program.cs
+++ garminImporter/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,64 @@ namespace garminImporter
     {
         static void Main(string[] args)
         {
+            GetFiles(@"C:\git\harristeq\garminImporter\allrides\");
         }
+
+        private static void GetFiles(string path)
+        {
+            var files = Directory.EnumerateFiles(path).Where(p => p.EndsWith(".tcx"));
+            XmlToJson(path + "test.tcx");
+
+            //foreach (var file in files)
+            //{
+            //    //var d = file;
+            //    XmlToJson(file);
+            //}
+        }
+
+        private static TrainingCenterInfo XmlToJson(string path)
+        {
+            var rtn = new TrainingCenterInfo();
+            var xml = string.Empty;
+
+            using (var file = new StreamReader(path))
+            {
+                xml = file.ReadToEnd();
+                file.Close();
+            }
+
+            
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            var nm = new XmlNamespaceManager(doc.NameTable);
+            nm.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            nm.AddNamespace("x", "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2");
+        
+            var json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+
+            var date = doc.SelectSingleNode("/x:TrainingCenterDatabase/x:Activities/x:Activity[1]/x:Id/text()", nm).Value;
+            var type = doc.SelectSingleNode("/x:TrainingCenterDatabase/x:Activities/x:Activity[1]/@Sport", nm).Value;
+
+            //Console.WriteLine("XML -> JSON: {0}", json);
+            //Console.ReadLine();
+
+            var dir = Path.GetDirectoryName(path);
+            var filename = Path.GetFileNameWithoutExtension(path);
+            var newPath = string.Format("{0}\\{1}.js", dir, filename);
+
+            //File.WriteAllText(newPath, json);
+
+
+            return rtn;
+        }
+
+        public class TrainingCenterInfo
+        {
+            public DateTime TrainingDate { get; set; }
+            public string FileName { get; set; }
+            public string ActivityType { get; set; }
+        }
+
     }
 }
--- garminImporter/db/TrainingCenterContext.cs
+++ garminImporter/db/TrainingCenterContext.cs
@@ -7,8 +7,71 @@ using System.Threading.Tasks;
 
 namespace garminImporter.db
 {
-    public class TrainingCenterContext : DbContext 
+    public class TrainingCenterContext : DbContext
     {
-
+        public DbSet<TrainingCenterDatabase_t> TrainingCenterDatabase_t { get; set; }
+        public DbSet<Folders_t> Folders_t { get; set; }
+        public DbSet<History_t> History_t { get; set; }
+        public DbSet<HistoryFolder_t> HistoryFolder_t { get; set; }
+        public DbSet<ActivityReference_t> ActivityReference_t { get; set; }
+        public DbSet<CoursePoint_t> CoursePoint_t { get; set; }
+        public DbSet<Position_t> Position_t { get; set; }
+        //public DbSet<CoursePointType_t> CoursePointType_t { get; set; }
+        public DbSet<Extensions_t> Extensions_t { get; set; }
+        public DbSet<CourseLap_t> CourseLap_t { get; set; }
+        public DbSet<HeartRateInBeatsPerMinute_t> HeartRateInBeatsPerMinute_t { get; set; }
+        public DbSet<HeartRateValue_t> HeartRateValue_t { get; set; }
+        public DbSet<HeartRateAsPercentOfMax_t> HeartRateAsPercentOfMax_t { get; set; }
+        //public DbSet<Intensity_t> Intensity_t { get; set; }
+        public DbSet<Course_t> Course_t { get; set; }
+        public DbSet<Trackpoint_t> Trackpoint_t { get; set; }
+        //public DbSet<SensorState_t> SensorState_t { get; set; }
+        public DbSet<AbstractSource_t> AbstractSource_t { get; set; }
+        public DbSet<Application_t> Application_t { get; set; }
+        public DbSet<Build_t> Build_t { get; set; }
+        public DbSet<Version_t> Version_t { get; set; }
+        //public DbSet<BuildType_t> BuildType_t { get; set; }
+        public DbSet<Device_t> Device_t { get; set; }
+        public DbSet<Zone_t> Zone_t { get; set; }
+        public DbSet<CustomHeartRateZone_t> CustomHeartRateZone_t { get; set; }
+        public DbSet<PredefinedHeartRateZone_t> PredefinedHeartRateZone_t { get; set; }
+        public DbSet<CustomSpeedZone_t> CustomSpeedZone_t { get; set; }
+        //public DbSet<SpeedType_t> SpeedType_t { get; set; }
+        public DbSet<PredefinedSpeedZone_t> PredefinedSpeedZone_t { get; set; }
+        public DbSet<Target_t> Target_t { get; set; }
+        public DbSet<None_t> None_t { get; set; }
+        public DbSet<Cadence_t> Cadence_t { get; set; }
+        public DbSet<HeartRate_t> HeartRate_t { get; set; }
+        public DbSet<Speed_t> Speed_t { get; set; }
+        public DbSet<Duration_t> Duration_t { get; set; }
+        public DbSet<UserInitiated_t> UserInitiated_t { get; set; }
+        public DbSet<CaloriesBurned_t> CaloriesBurned_t { get; set; }
+        public DbSet<HeartRateBelow_t> HeartRateBelow_t { get; set; }
+        public DbSet<HeartRateAbove_t> HeartRateAbove_t { get; set; }
+        public DbSet<Distance_t> Distance_t { get; set; }
+        public DbSet<Time_t> Time_t { get; set; }
+        public DbSet<AbstractStep_t> AbstractStep_t { get; set; }
+        public DbSet<Step_t> Step_t { get; set; }
+        public DbSet<Repeat_t> Repeat_t { get; set; }
+        public DbSet<Workout_t> Workout_t { get; set; }
+        //public DbSet<Sport_t> Sport_t { get; set; }
+        public DbSet<NextSport_t> NextSport_t { get; set; }
+        public DbSet<ActivityLap_t> ActivityLap_t { get; set; }
+        //public DbSet<TriggerMethod_t> TriggerMethod_t { get; set; }
+        public DbSet<Activity_t> Activity_t { get; set; }
+        public DbSet<Training_t> Training_t { get; set; }
+        public DbSet<QuickWorkout_t> QuickWorkout_t { get; set; }
+        public DbSet<Plan_t> Plan_t { get; set; }
+        //public DbSet<TrainingType_t> TrainingType_t { get; set; }
+        public DbSet<FirstSport_t> FirstSport_t { get; set; }
+        public DbSet<MultiSportSession_t> MultiSportSession_t { get; set; }
+        public DbSet<ActivityList_t> ActivityList_t { get; set; }
+        public DbSet<CourseFolder_t> CourseFolder_t { get; set; }
+        public DbSet<NameKeyReference_t> NameKeyReference_t { get; set; }
+        public DbSet<Courses_t> Courses_t { get; set; }
+        public DbSet<WorkoutFolder_t> WorkoutFolder_t { get; set; }
+        public DbSet<Workouts_t> Workouts_t { get; set; }
+        public DbSet<MultiSportFolder_t> MultiSportFolder_t { get; set; }
+        public DbSet<Week_t> Week_t { get; set; }
     }
 }
