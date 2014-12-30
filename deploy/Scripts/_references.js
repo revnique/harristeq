ca1da6e85e304ede59a9c94f545ea6cac0ecca38
Dominique <dharris@defisolutions.com>
2014-12-30 08:45:23 -0600

    before merge

--- deploy/Scripts/_references.js
+++ deploy/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-1.9.0.js" />
+﻿/// <reference path="jquery-2.1.1.js" />
 /// <reference path="jquery-ui-1.11.2.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
