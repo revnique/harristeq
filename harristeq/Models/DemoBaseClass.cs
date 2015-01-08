6085189ce28f7e0e4d3bb465ff84a87f0a6e28fd
Dominique <dharris@defisolutions.com>
2015-01-08 08:08:04 -0600

    started oop demo

---
+++ harristeq/Models/DemoBaseClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace harristeq.Models
+{
+    public class DemoBaseClass
+    {
+        public class DemoPublicClass
+        {
+        }
+
+        protected class DemoProtectedClass
+        {
+        }
+
+        private class DemoPrivateClass
+        {
+        }
+
+        public DemoPublicClass DemoPublicClassOut { get; set; }
+        protected DemoPublicClass DemoPublicClassProtected { get; set; }
+        private DemoPublicClass DemoPublicClassPrivate { get; set; }
+
+        protected DemoProtectedClass DemoProtectedClassOut { get; set; }
+        private DemoProtectedClass DemoProtectedClassPrivate { get; set; }
+        ////won't work
+        //public DemoProtectedClass DemoProtectedClassOutPublic { get; set; }
+
+        private DemoPrivateClass DemoPrivateClassOut { get; set; }
+        ////won't work
+        //public DemoPrivateClass DemoPrivateClassOutPublic { get; set; }
+        //protected DemoPrivateClass DemoPrivateClassOutProtected { get; set; }
+    }
+
+    public class DemoChildClass : DemoBaseClass
+    {
+    }
+
+    sealed class DemoSealedBaseClass
+    {
+        public void DoThis()
+        {
+            var d = new DemoAbstractChildClass();
+            d.MustHaveThis = "this";
+        }
+    }
+
+    ///// <summary>
+    ///// can't work
+    ///// </summary>
+    //public class DemoSealedChildClass : DemoSealedBaseClass
+    //{
+    //}
+
+    public abstract class DemoAbstractClass
+    {
+        public virtual string CanHaveThis { get; set; }
+        public abstract string MustHaveThis { get; set; }
+    }
+
+    public class DemoAbstractChildClass : DemoAbstractClass
+    {
+        public override string MustHaveThis { get; set; }
+    }
+
+}
