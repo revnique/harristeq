474e11f373933728430194f2f74feef62ef52119
Dominique <dharris@defisolutions.com>
2015-01-10 16:57:47 -0600

    added more styles to calendar started on ng-controller

--- harristeq/Scripts/home/calendarController.js
+++ harristeq/Scripts/home/calendarController.js
@@ -10,8 +10,29 @@
     //    $scope.$emit('showAjaxEvent', newValue);
     //});
     
+    
+    var colHeader = function(day, date, alert1, alert2, alert3) {
+        var self = this;
+        self.Day = day;
+        self.Date = date;
+        self.Alert1 = alert1;
+        self.Alert2 = alert2;
+        self.Alert3 = alert3;
+        return self;
+    };
+
+    var daysOfTheWeek = ["M","Tu","W","Th","F","Sa","Su"];
+
+    $scope.colHeaders = [];
+
+    var fillColHeaders = function(firstDay) {
+        for (var i = 0; i < 32; i++) {
+            
+        }
+    };
+
     $scope.init = function () {
-        
+
     };
 
     $scope.init();
