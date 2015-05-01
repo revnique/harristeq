5aabd4ef8ac1dbd01d24df90b2ccafea16ead08e
Dominique <dharris@defisolutions.com>
2015-05-01 04:08:50 -0500

    basics working

--- harristeq/Scripts/home/dorbaPhoneController.js
+++ harristeq/Scripts/home/dorbaPhoneController.js
@@ -1,4 +1,4 @@
-﻿harristeqApp.controller('dorbaPhoneController', function ($scope, harristeqSvc) {
+﻿harristeqApp.controller('dorbaPhoneController', function($scope, harristeqSvc) {
 
     var Trail = function(id, name, address, mapLink, latitude, longitude, currentStatus) {
         var self = this;
@@ -15,39 +15,125 @@
 
     $scope.trails = [];
 
-    $scope.getTrailMetadata = function (data) {
+    $scope.getTrailMetadata = function(data) {
         var trail;
         trail = new Trail("1", "Arbor Hills", "6701 W Parker Rd Plano, TX 75093", "http://maps.google.com/?q=33.05031+-96.850428", "33.05031", "-96.850428", 0);
         $scope.trails.push(trail);
-        
+
         console.log("$scope.trails", $scope.trails);
         console.log("data", data);
     };
 
+    $scope.getDorbaData = harristeqSvc.getTrailInfoNew().then(function(results) {
+        $scope.dorbaData = results;
+        console.log("dorbaData", results);
+    }, function() {
+        //fail goes here
+    });
 
     $scope.currentLatitude = "32.761195";
     $scope.currentLongitude = "-97.278058";
-        
 
 
+    $scope.init = function () {
 
-    $scope.init = function() {
-        var d = $scope.getJsBin;
+        //var d = $scope.getDorbaData;
         $scope.getTrailMetadata($scope.dorbaData);
 
         $(window).resize(this.onResize);
         this.onResize();
         window.addEventListener("orientationchange", this.onOrientationChange);
+
+
+        var states = new Bloodhound({
+            name: 'states',
+            datumTokenizer: function(d) {
+                return Bloodhound.tokenizers.whitespace(d.trailName);
+            },
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            // `states` is an array of state names defined in "The Basics"
+            local:
+            [
+                {
+             
+                        "trailId": "29",
+                        "trailName": "Tyler State Park",
+                        "openClose": "Closed",
+                        "currentStatus": "0",
+                        "currentCondition": "4",
+                        "conditionDesc": "Muddy",
+                        "geoLat": "32.472478",
+                        "geoLang": "-95.299187",
+                        "trailCity": "Tyler",
+                        "trailAddress": "789 Park Road 16 Tyler, TX 75706",
+
+                        "landOwner": "Tyler State Park Call 903-592-6790 for latest trail conditions",
+                        "facebook": "",
+                        "twitter": "",
+                        "updateFormatted": "Sun, Apr 19, 2015 @ 02:11 PM",
+                        "trailCategory": "4",
+                        "trailKML": null
+             
+                },
+                {
+                        "trailId": "30",
+                        "trailName": "Windmill Hill Preserve",
+                        "openClose": "Closed",
+                        "currentStatus": "0",
+                        "currentCondition": "4",
+                        "conditionDesc": "Muddy",
+                        "geoLat": "32.617280",
+                        "geoLang": "-96.908369",
+                        "trailCity": "DeSoto",
+                        "trailAddress": "",
+
+                        "landOwner": "",
+                        "facebook": "",
+                        "twitter": "",
+                        "updateFormatted": "Mon, Apr 13, 2015 @ 01:46 PM",
+                        "trailCategory": "2",
+                        "trailKML": null
+                 
+                }
+            ]
+        });
+        states.initialize();
+
+        //$('#txtPhoneSearch').typeahead({
+        //    hint: true,
+        //    highlight: true,
+        //    minLength: 1
+        //},
+        //{
+        //    name: 'states',
+        //    source: function(query, cb) {
+        //        data.get(query, function(suggestions) {
+        //            cb(filter(suggestions));
+        //        });
+        //    }
+        //});
+
+        $('#txtTabletSearch').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            name: 'states',
+            source: states.ttAdapter()
+        });
+
+
     };
 
-    $scope.onResize = _.debounce(function () {
+    $scope.onResize = _.debounce(function() {
         var h, w;
         w = $(window).innerWidth();
         h = $(window).innerHeight();
         return $scope.resize(w, h);
     }, 250);
 
-    $scope.onOrientationChange = function () {
+    $scope.onOrientationChange = function() {
         if (window.orientation === 0) {
         } else {
         }
@@ -58,11 +144,6 @@
     };
 
 
-    $scope.getJsBin = harristeqSvc.getTrailInfoNew().then(function (results) {
-        $scope.dorbaData = results;
-    }, function () {
-        //fail goes here
-    });
 
     var addZero = function(number) {
         var rtn = number + "";
@@ -85,105 +166,30 @@
     $scope.showError = false;
 
 
+    $scope.isSearchOpen = false;
 
-    $scope.onMenuClick = function (e) {
-        var self = this;
-        this.$mobileMenu = $("#mobile-menu");
 
-        if ($("body").hasClass("search-open")) {
-            $scope.onSearchClose();
-        }
+    var substringMatcher = function(strs) {
+        return function findMatches(q, cb) {
+            var matches, substringRegex;
 
-        if ($("body").hasClass("menu-open")) {
-            $("body").removeClass("menu-open");
+            // an array that will be populated with substring matches
+            matches = [];
 
-            setTimeout(function () {
-                if (self.$mobileMenu.hasClass("transition")) {
-                    self.$mobileMenu.removeClass("transition");
+            // regex used to determine if a string contains the substring `q`
+            substrRegex = new RegExp(q, 'i');
+
+            // iterate through the pool of strings and for any string that
+            // contains the substring `q`, add it to the `matches` array
+            $.each(strs, function(i, str) {
+                if (substrRegex.test(str)) {
+                    matches.push(str);
                 }
-                if (self.$mobileMenu.hasClass("visible")) {
-                    self.$mobileMenu.removeClass("visible");
-                }
-            }, 330);
+            });
 
-            return;
-
-        } else {
-
-            if (!this.$mobileMenu.hasClass("visible")) {
-                this.$mobileMenu.addClass("visible");
-            }
-
-            setTimeout(function () {
-                if (!self.$mobileMenu.hasClass("transition")) {
-                    self.$mobileMenu.addClass("transition");
-                }
-
-                setTimeout(function () {
-                    return $("body").addClass("menu-open");
-                }, 20);
-
-            }, 10);
-
-            return;
-        }
+            cb(matches);
+        };
     };
-
-
-    $scope.onSearchOpen = function () {
-        this.$el = $("#navigation");
-        this.$el.find(".search-item").addClass("active");
-        this.$el.find(".search-item").find(".icon").removeClass("search");
-        this.$el.find(".search-item").find(".icon").addClass("search-white");
-        this.isSearchActive = true;
-        if (!$("body").hasClass("search-open") && this.isHomePage !== true) {
-            $("body").addClass("search-open");
-            return true;
-        }
-        return true;
-    };
-
-    $scope.onSearchClose = function () {
-        this.$el.find(".search-item").removeClass("active");
-        this.$el.find(".search-item").find(".icon").removeClass("search-white");
-        this.$el.find(".search-item").find(".icon").addClass("search");
-        this.isSearchActive = false;
-        $("body").removeClass("search-open");
-        return;
-    };
-
-    $scope.onSearchClick = function (e) {
-        if ($(window).scrollTop() < 1 && this.isHomePage === true) {
-            return;
-        }
-        if ($("body").hasClass("menu-open")) {
-            $scope.onMenuClick();
-        }
-        if ($("body").hasClass("search-open")) {
-            return $scope.onSearchClose();
-        } else {
-            return $scope.onSearchOpen();
-        }
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     $scope.init();
